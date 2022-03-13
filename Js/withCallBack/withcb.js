934f22174afe90fee781932af2981701d0cd60b4
isanjeev23 <sk315055@gmail.com>
2022-03-13 12:42:01 +0530

    modf

--- Js/withCallBack/withcb.js
+++ Js/withCallBack/withcb.js
@@ -1,4 +1,4 @@
-// to run node withcb.js --source='f1.txt' --n= 50000
+// to run node withcb.js --source="f1.txt" --n=50000
 
 
 const minimist = require('minimist');
@@ -23,12 +23,12 @@ function checkPrime( n ){
 }
 
 let t1 = Date.now();
-console.log('Starting time task 1 is '+ t1 %10000);
+console.log('Starting time task 1 is '+ t1);
 
 // tast 1 read from file i.e disk related or is task k result p koi or dependen nhi h
 fs.readFile(args.source , function(data){
     let t2 = Date.now(); 
-    console.log('finishing time t2 is ' + t2 % 10000);
+    console.log('finishing time t2 is ' + t2 );
     console.log(t2-t1); // finishing tast 1 in milisec
     // this is also total time taken to complete task 1 and tast 2 
     // because tast 2 tast start parallarly
@@ -39,7 +39,7 @@ fs.readFile(args.source , function(data){
 
 
 let t3 = Date.now();
-console.log('starting time of task 2 is ' + t3%10000);
+console.log('starting time of task 2 is ' + t3);
 
 let arr  = [];
 
@@ -51,7 +51,7 @@ for(let i=2 ; i<=args.n ; i++){
     }
 }
 let t4 = Date.now();
-console.log('finishing time task2 is ' + t4 % 10000);
+console.log('finishing time task2 is ' + t4 );
 console.log(t4-t3);
 
 
