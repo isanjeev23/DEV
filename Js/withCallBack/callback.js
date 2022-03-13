c10c47422e84994c7d960f225d77e559a980bba6
isanjeev23 <sk315055@gmail.com>
2022-03-14 00:36:28 +0530

    cbFuncUpdated

--- Js/withCallBack/callback.js
+++ Js/withCallBack/callback.js
@@ -6,6 +6,7 @@ let args = minimist(process.argv);
 fs.readFile(args.source , 'utf-8', function(err, data){
     if (err) throw err;
 
-    fs.writeFileSync('f2.txt' , data);
+    console.log(err);
+    console.log(data);
    
    });
