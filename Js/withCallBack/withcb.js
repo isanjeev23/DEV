94afca6b0b48a48209b37d3721d6a3fb7e46e051
isanjeev23 <sk315055@gmail.com>
2022-03-13 09:07:55 +0530

    modif callback

--- Js/withCallBack/withcb.js
+++ Js/withCallBack/withcb.js
@@ -23,20 +23,23 @@ function checkPrime( n ){
 }
 
 let t1 = Date.now();
+console.log('Starting time task 1 is '+ t1 %10000);
 
 // tast 1 read from file i.e disk related or is task k result p koi or dependen nhi h
 fs.readFile(args.source , function(data){
     let t2 = Date.now(); 
-
+    console.log('finishing time t2 is ' + t2 % 10000);
     console.log(t2-t1); // finishing tast 1 in milisec
     // this is also total time taken to complete task 1 and tast 2 
     // because tast 2 tast start parallarly
-})
+});
 
 
 
 
 
+let t3 = Date.now();
+console.log('starting time of task 2 is ' + t3%10000);
 
 let arr  = [];
 
@@ -47,5 +50,8 @@ for(let i=2 ; i<=args.n ; i++){
         arr.push(i);
     }
 }
+let t4 = Date.now();
+console.log('finishing time task2 is ' + t4 % 10000);
+console.log(t4-t3);
 
 
