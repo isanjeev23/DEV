6c608a0c45b1dd15818b7ddec3d8ee9b2f5ea80c
isanjeev23 <sk315055@gmail.com>
2022-03-20 11:53:49 +0530

    readJson

--- Js/FirstJsonFile/readJson.js
+++ Js/FirstJsonFile/readJson.js
@@ -5,5 +5,14 @@ const fs = require('fs');
 
 let args = minimist(process.argv);
 
-let data = fs.readFileSync(args.source , 'utf-8');
-console.log(data);
+fs.readFile(args.source , 'utf-8' , (err , json )=>{
+    if(err){
+        console.log(err);
+    }else{
+        let teams = JSON.parse(json); // we need to parse the string throw json which
+        // provide array of Object not string 
+
+        console.log(teams[0].matches[0].result); // since teams is array of object we can access ..
+        // element of array with [] operator and property of element by . (opertor).
+    }
+})
